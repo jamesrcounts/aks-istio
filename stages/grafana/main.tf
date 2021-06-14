66a60da32d143b9a796dacbe1f44d1921b7e40cf
Jim Counts <jamesrcounts@outlook.com>
2021-06-14 14:27:18 +0000

    Fix grafana url

--- stages/grafana/main.tf
+++ stages/grafana/main.tf
@@ -1,5 +1,5 @@
 locals {
   grafana_auth = "${data.kubernetes_secret.grafana.data["admin-user"]}:${data.kubernetes_secret.grafana.data["admin-password"]}"
-  grafana_url  = "http://${data.kubernetes_service.grafana.status.0.load_balancer.0.ingress.0.ip}:9090"
+  grafana_url  = "http://${data.kubernetes_service.grafana.status.0.load_balancer.0.ingress.0.ip}"
   kube_config  = data.azurerm_kubernetes_cluster.aks.kube_admin_config.0
 }
