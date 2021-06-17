fd2c0343ff43511d2346ac4410f57d5c51acdc8a
Jim Counts <jamesrcounts@outlook.com>
2021-06-17 13:48:54 +0000

    Istio and isto-operator installs

--- stages/kubernetes/istio.tf
+++ stages/kubernetes/istio.tf
@@ -1,31 +1,9 @@
-// https://istio.io/latest/docs/setup/install/helm/
-// helm install istio-base manifests/charts/base -n istio-system
-resource "helm_release" "istio_base" {
-  name      = "istio-base"
-  chart     = "../../charts/istio-1.10.1/manifests/charts/base"
-  namespace = kubernetes_namespace.istio_system.metadata.0.name
+data "kustomization" "istio" {
+  path = "./istio"
 }
 
-// helm install istiod manifests/charts/istio-control/istio-discovery \
-//     -n istio-system
-resource "helm_release" "istiod" {
-  name      = "istiod"
-  chart     = "../../charts/istio-1.10.1/manifests/charts/istio-control/istio-discovery"
-  namespace = kubernetes_namespace.istio_system.metadata.0.name
-}
-
-// helm install istio-ingress manifests/charts/gateways/istio-ingress \
-//     -n istio-system
-resource "helm_release" "istio_ingress" {
-  name      = "istio-ingress"
-  chart     = "../../charts/istio-1.10.1/manifests/charts/gateways/istio-ingress"
-  namespace = kubernetes_namespace.istio_system.metadata.0.name
-}
-
-// helm install istio-egress manifests/charts/gateways/istio-egress \
-//     -n istio-system
-resource "helm_release" "istio_egress" {
-  name      = "istio-egress"
-  chart     = "../../charts/istio-1.10.1/manifests/charts/gateways/istio-egress"
-  namespace = kubernetes_namespace.istio_system.metadata.0.name
+resource "kustomization_resource" "istio" {
+  for_each   = data.kustomization.istio.ids
+  manifest   = data.kustomization.istio.manifests[each.value]
+  depends_on = [kustomization_resource.istio_operator]
 }
