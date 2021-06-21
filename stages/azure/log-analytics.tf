3664183519f158e4adb03c82e271b1089139b8dc
Jim Counts <jamesrcounts@outlook.com>
2021-06-21 13:34:52 +0000

    Container insights now provided by LP

--- stages/azure/log-analytics.tf
+++ stages/azure/log-analytics.tf
@@ -3,16 +3,3 @@ data "azurerm_log_analytics_workspace" "logs" {
   resource_group_name = data.azurerm_resource_group.backend.name
 }
 
-resource "azurerm_log_analytics_solution" "insights" {
-  solution_name         = "ContainerInsights"
-  location              = var.log_analytics_workspace.location
-  resource_group_name   = var.log_analytics_workspace.resource_group_name
-  workspace_resource_id = var.log_analytics_workspace.id
-  workspace_name        = var.log_analytics_workspace.name
-  tags                  = var.resource_group.tags
-
-  plan {
-    publisher = "Microsoft"
-    product   = "OMSGallery/ContainerInsights"
-  }
-}
