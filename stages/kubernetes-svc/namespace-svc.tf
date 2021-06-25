b2c69cf6920c80288b64dd22b0793b649fc070d6
Jim Counts <jamesrcounts@outlook.com>
2021-06-25 13:19:31 +0000

    Add problematic resources outside module

--- stages/kubernetes-svc/namespace-svc.tf
+++ stages/kubernetes-svc/namespace-svc.tf
@@ -4,3 +4,24 @@ module "svc_namespace" {
 
   name = "svcs"
 }
+
+data "kustomization_build" "svc_istio_configuration" {
+  path = "${path.module}/svc-istio-configuration"
+}
+
+resource "kustomization_resource" "svc_istio_configuration" {
+  for_each = data.kustomization_build.svc_istio_configuration.ids
+  manifest = data.kustomization_build.svc_istio_configuration.manifests[each.value]
+}
+
+// resource "kubernetes_network_policy" "default_deny_all" {
+//   metadata {
+//     name      = "default-deny-all"
+//     namespace = "svcs"
+//   }
+
+//   spec {
+//     pod_selector {}
+//     policy_types = ["Ingress", "Egress"]
+//   }
+// }
