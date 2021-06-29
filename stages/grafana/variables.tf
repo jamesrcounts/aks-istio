0cd6a2ef10c587d69c349a1cc4785e34ab76796d
Jim Counts <jamesrcounts@outlook.com>
2021-06-29 13:16:58 +0000

    Cleanup

--- stages/grafana/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aks_cluster_name" {
-  description = "(Required) The name of the AKS cluster to configure."
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "(Required) The name of the resource group for this project."
-  type        = string
-}
